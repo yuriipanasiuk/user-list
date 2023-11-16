375e392f804e9ec4a7ee7cf33f9e13b6804910e6
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-11-16 15:56:44 +0200

    fix base url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/user-list">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: production ? "[name].[contenthash].js" : "[name].js",
-    publicPath: "/user-list",
+    publicPath: "/",
   },
   module: {
     rules: [
